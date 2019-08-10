5d479e655963bf7f40ca91f3198343b8c0362985
Tobias Möritz <tobias@moeritz.cc>
2019-08-10 23:03:31 +0200

    🚧

--- plugin/main.js
+++ plugin/main.js
@@ -1,154 +1,62 @@
-var websocket = null;
-var pluginUUID = null;
-var settingsCache = {};
+let websocket = null,
+    pluginUUID = null;
+    makerkey = "";
 
-var DestinationEnum = Object.freeze({"HARDWARE_AND_SOFTWARE":0, "HARDWARE_ONLY":1, "SOFTWARE_ONLY":2})
-
-var webhookAction = {
-
-    type : "de.tobimori.streamdeck.ifttt.action",
-
-    onKeyDown : function(context, settings, coordinates, userDesiredState) {
-    },
-
-    onKeyUp : function(context, settings, coordinates, userDesiredState) {
-        settingsCache[context] = settings;
-        var eventname = "";
-        if(settings != null && settings.hasOwnProperty('eventname')){
-            eventname = settings["eventname"];
-        }
-        var makerkey = "";
-        if(settings != null && settings.hasOwnProperty('makerkey')){
-            makerkey = settings["makerkey"];
-        }
-        if(eventname == "") {
-            this.ShowReaction(context, "Alert");
-        } else if(makerkey == "") {
-            this.ShowReaction(context, "Alert");
-        } else {
-            const request = new XMLHttpRequest();
-            request.open("GET", 'https://maker.ifttt.com/trigger/' + eventname + '/with/key/' + makerkey);
-            request.send();
-        }
-
-    },
-
-    onWillAppear : function(context, settings, coordinates) {
-        settingsCache[context] = settings;
-        var eventname = "";
-        if(settings != null && settings.hasOwnProperty('eventname')){
-            eventname = settings["eventname"];
-        }
-        var makerkey = "";
-        if(settings != null && settings.hasOwnProperty('makerkey')){
-            makerkey = settings["makerkey"];
-        }
-        if(eventname == "") {
-            this.ShowReaction(context, "Alert");
-        } else if(makerkey == "") {
-            this.ShowReaction(context, "Alert");
-        }
-    },
-
-    ShowReaction : function(context, type) {
-        var json = {
-            "event": "show" + type,
-            "context": context,
-        };
-        websocket.send(JSON.stringify(json));
-    },
-
-    SetSettings : function(context, settings) {
-        var json = {
-            "event": "setSettings",
-            "context": context,
-            "payload": settings
-        };
-
-        websocket.send(JSON.stringify(json));
-    },
-
-    SendSettings : function(action, context) {
-        var json = {
-            "action": action,
-            "event": "sendToPropertyInspector",
-            "context": context,
-            "payload": settingsCache[context]
-        };
-
-        websocket.send(JSON.stringify(json));
-    }
-};
-
-function connectSocket(inPort, inPluginUUID, inRegisterEvent, inInfo)
+function connectElgatoStreamDeckSocket(inPort, inPluginUUID, inRegisterEvent, inInfo)
 {
     pluginUUID = inPluginUUID;
 
     // Open the web socket
     websocket = new WebSocket("ws://localhost:" + inPort);
 
-    function registerPlugin(inPluginUUID)
+    websocket.onopen = function()
     {
-        var json = {
+        // WebSocket is connected, register the plugin
+        const json = {
             "event": inRegisterEvent,
             "uuid": inPluginUUID
         };
-
+    
         websocket.send(JSON.stringify(json));
-    };
-
-    websocket.onopen = function()
-    {
-        // WebSocket is connected, send message
-        registerPlugin(pluginUUID);
     };
 
     websocket.onmessage = function (evt)
     {
         // Received message from Stream Deck
-        var jsonObj = JSON.parse(evt.data);
-        var event = jsonObj['event'];
-        var action = jsonObj['action'];
-        var context = jsonObj['context'];
-        var jsonPayload = jsonObj['payload'];
-
-        if(event == "keyDown")
-        {
-            var settings = jsonPayload['settings'];
-            var coordinates = jsonPayload['coordinates'];
-            var userDesiredState = jsonPayload['userDesiredState'];
-            console.log(settings);
-            webhookAction.onKeyDown(context, settings, coordinates, userDesiredState);
-        }
-        else if(event == "keyUp")
-        {
-            var settings = jsonPayload['settings'];
-            var coordinates = jsonPayload['coordinates'];
-            var userDesiredState = jsonPayload['userDesiredState'];
-            webhookAction.onKeyUp(context, settings, coordinates, userDesiredState);
-        }
-        else if(event == "willAppear")
-        {
-            var settings = jsonPayload['settings'];
-            var coordinates = jsonPayload['coordinates'];
-            webhookAction.onWillAppear(context, settings, coordinates);
-        }
-        else if(event == "sendToPlugin") {
-
-            if(jsonPayload['type'] == "updateSettings") {
-
-                webhookAction.SetSettings(context, jsonPayload);
-                settingsCache[context] = jsonPayload;
-
-            } else if(jsonPayload['type'] == "requestSettings") {
-
-                webhookAction.SendSettings(action, context);
+        const jsonObj = JSON.parse(evt.data);
+        console.log(jsonObj);
+        
+        if(jsonObj['event'] == "keyUp")
+        {            
+            let eventname = "";
+            if(jsonObj.payload.settings != null && jsonObj.payload.settings.hasOwnProperty('eventname')){
+                eventname = jsonObj.payload.settings["eventname"];
             }
-        }
-    };
 
-    websocket.onclose = function()
-    {
-        // Websocket is closed
+            if(eventname == "" || makerkey == "") {
+                const json = {
+                    "event": "showAlert",
+                    "context": jsonObj.context,
+                };
+                websocket.send(JSON.stringify(json));
+            } else {
+                const request = new XMLHttpRequest();
+                request.open("GET", 'https://maker.ifttt.com/trigger/' + eventname + '/with/key/' + makerkey);
+                request.send();
+            };
+
+        } else if(jsonObj['event'] == "didReceiveGlobalSettings") {
+            if(jsonObj.payload.settings != null && jsonObj.payload.settings.hasOwnProperty('makerkey')){
+                makerkey = jsonObj.payload.settings["makerkey"];
+            }
+
+        } else if(jsonObj['event'] == "keyDown") {
+            const json = {
+                "event": "getGlobalSettings",
+                "context": pluginUUID
+            };
+    
+            websocket.send(JSON.stringify(json));
+        }    
     };
 };
