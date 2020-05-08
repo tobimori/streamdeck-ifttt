e23d5bdc2a2a6dd259f4277be081b3a85cfb9703
tobimori <tobias@moeritz.cc>
2020-05-08 16:24:15 +0200

    🔖 v2.0 release

--- plugin/main.js
+++ plugin/main.js
@@ -1,39 +1,35 @@
 let websocket = null,
     pluginUUID = null;
-    makerkey = "";
+makerkey = "";
 
-function connectElgatoStreamDeckSocket(inPort, inPluginUUID, inRegisterEvent, inInfo)
-{
+function connectElgatoStreamDeckSocket(inPort, inPluginUUID, inRegisterEvent, inInfo) {
     pluginUUID = inPluginUUID;
 
     // Open the web socket
     websocket = new WebSocket("ws://localhost:" + inPort);
 
-    websocket.onopen = function()
-    {
+    websocket.onopen = function () {
         // WebSocket is connected, register the plugin
         const json = {
             "event": inRegisterEvent,
             "uuid": inPluginUUID
         };
-    
+
         websocket.send(JSON.stringify(json));
     };
 
-    websocket.onmessage = function (evt)
-    {
+    websocket.onmessage = function (evt) {
         // Received message from Stream Deck
         const jsonObj = JSON.parse(evt.data);
         console.log(jsonObj);
-        
-        if(jsonObj['event'] == "keyUp")
-        {            
+
+        if (jsonObj['event'] == "keyUp") {
             let eventname = "";
-            if(jsonObj.payload.settings != null && jsonObj.payload.settings.hasOwnProperty('eventname')){
+            if (jsonObj.payload.settings != null && jsonObj.payload.settings.hasOwnProperty('eventname')) {
                 eventname = jsonObj.payload.settings["eventname"];
             }
 
-            if(eventname == "" || makerkey == "") {
+            if (eventname == "" || makerkey == "") {
                 const json = {
                     "event": "showAlert",
                     "context": jsonObj.context,
@@ -45,18 +41,18 @@ function connectElgatoStreamDeckSocket(inPort, inPluginUUID, inRegisterEvent, in
                 request.send();
             };
 
-        } else if(jsonObj['event'] == "didReceiveGlobalSettings") {
-            if(jsonObj.payload.settings != null && jsonObj.payload.settings.hasOwnProperty('makerkey')){
+        } else if (jsonObj['event'] == "didReceiveGlobalSettings") {
+            if (jsonObj.payload.settings != null && jsonObj.payload.settings.hasOwnProperty('makerkey')) {
                 makerkey = jsonObj.payload.settings["makerkey"];
             }
 
-        } else if(jsonObj['event'] == "keyDown") {
+        } else if (jsonObj['event'] == "keyDown") {
             const json = {
                 "event": "getGlobalSettings",
                 "context": pluginUUID
             };
-    
+
             websocket.send(JSON.stringify(json));
-        }    
+        }
     };
 };
